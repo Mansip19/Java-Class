05468c7868a3c9e61c31ac71e8764d2e407feeeb
Mansip19 <162776198+Mansip19@users.noreply.github.com>
2024-04-02 19:13:13 +0530

    Update ArrayInJava5.java

--- ArrayInJava5.java
+++ ArrayInJava5.java
@@ -3,13 +3,9 @@ package com.javacore.demo;
 public class ArrayInJava5 {
 
 	public static void main(String[] args) {
-		int[] a = {12, 21, 0, 5, 7};   //Declaring and initializing an array of ints
-		 
-        //Creating an array object of same length as array 'a'
+		int[] a = {12, 21, 0, 5, 7}
  
         int[] b = new int[a.length];
-        
-      //creating a copy of array 'a' using arraycopy() method of System class
         
         System.arraycopy(a, 0, b, 0, a.length);
         
@@ -17,8 +13,6 @@ public class ArrayInJava5 {
 		{
 			System.out.println(b[i]);
 		}
-        //change the value of an array and see the result
-        // print the array
         
 	}
 
